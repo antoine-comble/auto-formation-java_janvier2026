84a0778b666eec8e1d28c07aa56a77ce25a615fc
Antoine <antoine.comble@zenika.com>
2026-01-07 16:23:41 +0100

    Anagram exercise

---
+++ src/main/com/zenika/exercises/App.java
@@ -0,0 +1,41 @@
+package main.com.zenika.exercises;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.List;
+
+import static main.com.zenika.exercises.Utils.anagramFrom;
+import static main.com.zenika.exercises.Utils.readDictionary;
+
+public class App {
+
+    static void main(final String[] args) throws IOException, URISyntaxException {
+        List<String> data = readDictionary();
+
+        String anagram = anagramFrom(data);
+        System.out.println("anagram = " + anagram);
+
+        String result = decodeAnagram(anagram, data);
+        System.out.println("result = " + result);
+    }
+
+    static String decodeAnagram(String anagram, List<String> data) {
+        for (int i = 0; i < data.size(); i++) {
+            if (equalsIgnoreOrder(anagram, data.get(i))) {
+                return data.get(i);
+            }
+        }
+        throw new RuntimeException("Anagram " + anagram + " not found");
+    }
+
+    public static boolean equalsIgnoreOrder(String anagram, String word) {
+        char[] anagramCharArray = anagram.toCharArray();
+        char[] wordCharArray = word.toCharArray();
+
+        Arrays.sort(anagramCharArray);
+        Arrays.sort(wordCharArray);
+
+        return Arrays.equals(anagramCharArray, wordCharArray);
+    }
+}
