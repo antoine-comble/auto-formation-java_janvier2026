47441b09ea9cb99623b2d32a1d4172ade3a709d3
Antoine <antoine.comble@zenika.com>
2026-01-08 14:59:43 +0100

    Exercise count7

--- src/main/com/zenika/tp/java/Exercises.java
+++ src/main/com/zenika/tp/java/Exercises.java
@@ -142,4 +142,15 @@ public class Exercises {
         if (n == 0) return 0;
         return sumDigits(n / 10) + (n % 10);
     }
+
+    /*
+     * Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2 (no loops).
+     * Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+     */
+    public static int count7(int n) {
+        if (n == 0) return 0;
+        int result = count7(n / 10);
+        if (n % 10 == 7) return result + 1;
+        return result;
+    }
 }
